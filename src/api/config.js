00763637f1b6d897578e05148d72e07c691ae8e6
Fenimore Baena <fenimorebvsennin@gmail.com>
2017-09-25 15:04:20 -0500

    Initial commit

---
+++ src/api/config.js
@@ -0,0 +1,6 @@
+export  default {
+    appName:    'fenimusic',
+    apiKey:     '91361f0b8aca35304a0407cc5b901b9d',
+    secret:     '5d6b0ed6660245c8a872b8e86a6455a5',
+    registeredTo:'fenimore92'
+}
